c86ce1780a34af2c25d2be491e0423b9dbe28887
Ladislav Pataki <ladislav.pataki@outlook.com>
2020-07-17 10:54:34 +0200

    added GlobalAssemblyInfo

---
+++ src/GlobalAssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Company name")]
+[assembly: AssemblyCopyright("© Copyright 2020 by Company. All rights reserved.")]
+[assembly: AssemblyTrademark("Product name is a registered trademark of Company name.")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyInformationalVersion("0.0-alpha")]
+[assembly: AssemblyProduct("Product name")]
+[assembly: AssemblyDescription("Description")]
+
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
