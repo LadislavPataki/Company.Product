4fba63c626112eace02fdbf7be813a4dc209feb7
Ladislav Pataki <LadislavPataki@users.noreply.github.com>
2020-08-07 09:18:10 +0200

    Update GlobalAssemblyInfo.cs

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -2,11 +2,10 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Company name")]
-[assembly: AssemblyCopyright("© Copyright 2020 by Company. All rights reserved.")]
+[assembly: AssemblyCopyright("Â© Copyright 2020 by Company. All rights reserved.")]
 [assembly: AssemblyTrademark("Product name is a registered trademark of Company name.")]
 [assembly: AssemblyVersion("0.0.1.0")]
 [assembly: AssemblyFileVersion("0.0.1.0")]
-[assembly: AssemblyInformationalVersion("0.0-alpha")]
 [assembly: AssemblyProduct("Product name")]
 [assembly: AssemblyDescription("Description")]
 
