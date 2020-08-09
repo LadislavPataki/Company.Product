dd1979c5e132f2afc9e875d81218e7e30fa05180
Ladislav Pataki <pataki.ladislav@gmail.com>
2020-08-09 08:44:38 +0200

    Test result

--- src/Company.Product.UI.Tests/UnitTest1.cs
+++ src/Company.Product.UI.Tests/UnitTest1.cs
@@ -6,9 +6,20 @@ namespace Company.Product.UI.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestShouldFail()
         {
+            var expected = 1;
+            var actual = 0;
 
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TestShouldPass()
+        {
+            var expected = 1;
+            var actual = 1;
+
+            Assert.Equal(expected, actual);
         }
     }
 }
